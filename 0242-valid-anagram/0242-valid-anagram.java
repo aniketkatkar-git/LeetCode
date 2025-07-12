b0ccca1d34fcb9b54ce5044073f0539d05dc7693
Aniket Katkar <61575046+aniketkatkar-git@users.noreply.github.com>
2025-07-12 11:05:32 +0530

    Time: 4 ms (77.32%), Space: 43.7 MB (58.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
+        }
+
+        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+
+        return allZeros;
+    }
+}
